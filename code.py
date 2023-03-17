44f3a84da10aca2b68be9f8aa917eddb796b5299
Ruchi Sharma <statsnotes21@gmail.com>
2023-03-17 18:02:08 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,20 @@
+from turtle import *
 
+def tree(branchLen):
+  if branchLen > 5:
+    forward(branchLen)
+    right(20)
+    tree(branchLen-15)
+    left(40)
+    tree(branchLen-15)
+    right(20)
+    backward(branchLen)
+   
+myWin = Screen()
+left(90)
+up()
+backward(100)
+down()
+color('green')
+tree(75)
+myWin.exitonclick()
